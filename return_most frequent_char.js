5decf723150b0fb4c2b85f6ffa993dde9be909a7
Paul Guillermo Ottenwalder <paulguillermo19@gmail.com>
2022-10-05 21:28:05 -0400

    Update return_most frequent_char.js

--- return_most frequent_char.js
+++ return_most frequent_char.js
@@ -5,10 +5,13 @@ let s2 = 'ABBCCCDDDD'
 let solution = function(s){
   let chars = []
   for (let char of s){
+    // check if the char is already in the array
     let isInChar = chars.find(letter => letter[char])
+    //     if not in array added
     if (!isInChar){
       chars.push({[char]: 1})
     } else {
+      //       since is in the array +1 to this char
       chars.forEach(letter => {
         if (letter[char]){
           letter[char] += 1
@@ -17,6 +20,7 @@ let solution = function(s){
     }    
   }
 
+  //   find the most frequent char in chars array
   let mostFrequentCharCount = 0
   let mostFrequentChar = 0
   for (let char of s){
